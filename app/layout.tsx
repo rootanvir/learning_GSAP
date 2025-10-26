066b5934f67f7367b8514b9128afde9269a96298
rootanvir <tanvir.22464751@gmail.com>
2025-10-26 23:20:04 +0600

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ interface Props {
 const RootLayer: React.FC<Props> = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="ico" type="images/svg+xml" href='/images/logo.png'/>
+      </head>
       <body className="bg-white">
         <div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import React, { FC } from 'react';
 import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Nav from '../components/Nav'
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 interface Props {
@@ -10,11 +11,9 @@ interface Props {
 
 const Page: FC<Props> = ({  }) => {
   return (
-    <div className='flex justify-center'>
-      <h1 className='text-3xl text-indigo-300'>
-          Hello GSAP
-      </h1>
-    </div>
+    <main>
+      <Nav />
+    </main>
   );
 };
 
